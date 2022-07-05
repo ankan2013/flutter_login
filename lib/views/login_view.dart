02c763dc93b657ca9093ebef1915225e84739581
ankan2013 <ankan2013@gmail.com>
2022-07-05 23:05:00 +0300

    Initial commit

---
+++ lib/views/login_view.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:supabase/supabase.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({Key? key}) : super(key: key);
+
+  @override
+  _LoginViewState createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _buildBody(context),
+      backgroundColor: Color(0XFF181818),
+    );
+  }
+
+  Widget _buildBody(context) {
+    final size = MediaQuery.of(context).size;
+
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            child: Card(
+              elevation: 1,
+              child: Container(
+                padding: EdgeInsets.only(bottom: 40),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 20.0),
+                        child: TextFormField(
+                          controller: _emailController,
+                          cursorColor: Colors.black,
+                          style: TextStyle(),
+                          decoration: InputDecoration(
+                            labelText: 'E-mail',
+                            labelStyle: TextStyle(color: Colors.black),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: new BorderSide(color: Colors.black),
+                            ),
+                          ),
+                          validator: (String? value) {
+                            if (value!.isEmpty || !value.contains('@')) {
+                              return 'Invalid e-mail';
+                            }
+                          },
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 20.0),
+                        child: TextFormField(
+                          controller: _passwordController,
+                          cursorColor: Colors.black,
+                          obscureText: true,
+                          style: TextStyle(),
+                          decoration: InputDecoration(
+                            labelText: 'Password',
+                            labelStyle: TextStyle(color: Colors.black),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: new BorderSide(color: Colors.black),
+                            ),
+                          ),
+                          validator: (String? value) {
+                            if (value!.isEmpty) {
+                              return 'Invalid password';
+                            }
+                          },
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        width: size.width,
+                        height: 45,
+                        child: MaterialButton(
+                          onPressed: () async {
+                            if (_formKey.currentState!.validate()) {
+                              _login();
+                            }
+                          },
+                          color: Colors.black,
+                          child: Text(
+                            'Login',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('Don\'t have an account?'),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.pushNamed(context, '/register');
+                            },
+                            child: Text(
+                              'Register',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Future _login() async {
+    final sharedPreferences = await SharedPreferences.getInstance();
+
+    final result = await GetIt.I.get<SupabaseClient>().auth.signIn(
+        email: _emailController.text, password: _passwordController.text);
+
+    if (result.data != null) {
+      await sharedPreferences.setString(
+          'user', result.data!.persistSessionString);
+      Navigator.pushReplacementNamed(context, '/home');
+    } else if (result.error?.message != null) {
+      _showDialog(context, title: 'Error', message: result.error?.message);
+    }
+  }
+}
+
+void _showDialog(context, {String? title, String? message}) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: new Text(title ?? ''),
+        content: new Text(message ?? ''),
+        actions: <Widget>[
+          new MaterialButton(
+            child: new Text("Close"),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
